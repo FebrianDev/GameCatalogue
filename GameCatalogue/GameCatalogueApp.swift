f310c9fdbb4faac11fc6d8b91beb4d77d54d90ef
febriandev <febrian26022001@gmail.com>
2024-03-27 02:14:43 +0700

    Initial Commit

---
+++ GameCatalogue/GameCatalogueApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameCatalogueApp.swift
+//  GameCatalogue
+//
+//  Created by Febrian on 27/03/24.
+//
+
+import SwiftUI
+
+@main
+struct GameCatalogueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
